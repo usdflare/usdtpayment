1eeab3abb6a9fb19e88bf9bc0165e26c3f97e55e
Katie <usdflare@outlook.com>
2024-04-26 09:14:16 +0800

    Update native.php
    update

--- php/native.php
+++ php/native.php
@@ -24,6 +24,27 @@ $gatewayUrl = $config['gatewayUrl']; //usdflare网关地址
 $signstr = $appid . $apikey . $orderid . $money . $type;
 $sign = hash('sha256', $signstr);
 //构建请求地址
-$nativeurl = $gatewayUrl . '?appid=' . $appid . '&orderid=' . $orderid . '&money=' . $money . '&type=' . $type .'&sign=' . $sign;
-die(header("Location: $nativeurl"));
+$nativeurl = $gatewayUrl . '?appid=' . $appid . '&orderid=' . $orderid . '&money=' . $money . '&type=' . $type . '&sign=' . $sign;
+//get方式请求直接跳转
+// die(header("Location: $nativeurl"));
+?>
 
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <form id="payform" hidden="hidden" action="<?=$config['gatewayUrl']?>" method="post">
+        <input type="text" name="appid" value='<?= $appid ?>'>
+        <input type="text" name="orderid" value='<?= $orderid ?>'>
+        <input type="text" name="money" value='<?= $money ?>'>
+        <input type="text" name="type" value='<?= $type ?>'>
+        <input type="text" name="sign" value='<?= $sign ?>'>
+    </form>
+</body>
+
+<script>
+    document.getElementById('payform').submit();
+</script>
